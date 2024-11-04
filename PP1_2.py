beaa4cff95b30218f35a43d2d387837eafd4ca84
Dinara-Pathiraja-ICD2O1-02 <Dinara.pathiraja@student.tdsb.on.ca>
2024-11-04 17:35:14 -0500

    Update PP1_2.py

--- PP1_2.py
+++ PP1_2.py
@@ -8,39 +8,39 @@
 
 
 def q1():
-  name = "Alex"
-  print (name)
+    name = "Alex"
+    print (name)
 
 def q2():
-  num1 = 3
-  num2 = 5
-  num3 = 2
-  print (num1 + num2 + num3)
+    num1 = 3
+    num2 = 5
+    num3 = 2
+    print (num1 + num2 + num3)
 
 def q3():
-  num1 = 21
-  num2 = 4
-  print(num1 // num2)
+    num1 = 21
+    num2 = 4
+    print(num1 // num2)
 
 def q4():
-  num1 = 6.3
-  num2 = 3.0
-  print(num1 / num2)
+    num1 = 6.3
+    num2 = 3.0
+    print(num1 / num2)
 
 def q5():
-  word = "Hello World"
-  print(word)
-  Word = "Goodbye World" 
-  print(word)
+    word = "Hello World"
+    print(word)
+    Word = "Goodbye World" 
+    print(word)
 
 def q6():
     bool1 = "True"
     print(bool1)
 
 def q7():
-  num1 = 7
-  num2 = 2
-  print(num1 % num2)
+    num1 = 7
+    num2 = 2
+    print(num1 % num2)
 
   
 #DO NOT EDIT ANYTHING AFTER THIS COMMENT
